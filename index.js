3a33e81021fd18d1d4c9d79b5eb78cd73b2dde92
JayFerreira <jayzoca@gmail.com>
2019-05-02 17:38:27 -0300

    inclusão de imput com números, testes com numeros e pontuação, atualização do readme

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function verification(stringCardNumber){
+function cardValidator(stringCardNumber){
     let cardNumber = stringCardNumber.split("").map(Number);
      if (cardNumber.length < 16 || cardNumber.length > 16 || cardNumber.every(digit => digit === cardNumber[0])) {
-       console.log("cartão inválido");
+      //  console.log("cartão inválido");
          return false
        } else {
-         console.log("cartão válido");
+        //  console.log("cartão válido");
          luhnAlgorithm(cardNumber);
-       //  return true 
-       } 
-       
+        return true 
+       }       
     
    }
    
@@ -31,26 +30,24 @@ function verification(stringCardNumber){
    newArray.push(value);
      }
    }
-   console.log(newArray);
-   cardValidator(newArray, checkDigit);
+  //  console.log(newArray);
+   finalOperation(newArray, checkDigit);
    return newArray;
    }
    
-    function cardValidator(newArray, checkDigit){
+    function finalOperation(newArray, checkDigit){
      let finalsum = newArray.reduce((acc, elem) =>{
         return acc + elem
        },)
-       console.log(finalsum);
-   
-     let contaDoModulo = 10 - (finalsum % 10);
-     console.log(contaDoModulo);
-   
-    if (checkDigit === contaDoModulo){
-       console.log("cartão válido");
+        
+     let mod = 10 - (finalsum % 10);
+      
+    if (checkDigit === mod){
+      return true
     }else{
-     console.log("cartão inválido");
+      return false
     } 
    }
    
-   // verification("5253193799067539");
-   verification("4916364827547055");
+    module.exports.cardValidator = cardValidator;
+
--- test/cardvalidator.spec.js
+++ test/cardvalidator.spec.js
@@ -1,1 +1,27 @@
-  
+const chai = require("chai");
+const expect = chai.expect;
+const cardNumber = require("../index");
+const card = cardNumber.cardValidator;
+
+
+describe("Card Validation", () => {
+    it('Should return false for equal numbers', () => {
+        expect(card('1111111111111111')).to.equal(false);
+    });
+
+    it("Should return invalid for longer than 11 digits", () => {
+        expect(card('11111111111111111111111')).to.equal(false);
+    });
+    it("Should return invalid for shorter than 11 digits", () => {
+        expect(card('111111')).to.equal(false);
+    });
+   
+});
+
+
+
+
+
+
+
+
